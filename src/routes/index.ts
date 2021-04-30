db5b376313f6623aa03b0f68fed71a5bfdc55a38
ardeshirsaadat <ardeshirsaadat@gmail.com>
2021-04-30 10:09:53 -0400

    add types to express routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import { images } from './api/images'
 const routes = express.Router()
-routes.get('/', (req, res) => {
+routes.get('/', (req:express.Request, res:express.Response) => {
     res.send('this is the landing page')
 })
 routes.use('/images', images)
